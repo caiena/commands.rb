bcadccc3f37c0c1cad0851d3b420d727db0ca183
Mateus Sabino <mateussabino87@gmail.com>
2023-03-09 14:00:05 -0300

    feat: Adiciona testes para o módulo Commands::Commander

--- spec/commands/commander_spec.rb
+++ spec/commands/commander_spec.rb
@@ -1,4 +1,75 @@
 # frozen_string_literal: true
 
-RSpec.describe "Commands::Commander" do 
+require "./lib/commands/commander"
+
+RSpec.describe Commands::Commander do
+  describe "methods" do
+    describe "command" do
+      class ExampleCommand < Commands::Command
+        attribute :attr1
+
+        def call
+          "some value"
+        end
+      end
+
+      class ExampleKlass
+        include Commands::Commander
+
+        command :example_command,
+                args: ->(instance) { { attr1: instance } },
+                class_name: "ExampleCommand"
+      end
+
+      let(:example_klass) { ExampleKlass.new }
+
+      describe "methods" do
+        it "exposes commands as instance methods with bang" do
+          expect(example_klass).to respond_to(:example_command!)
+        end
+
+        it "exposes commands as instance methods without bang" do
+          expect(example_klass).to respond_to(:example_command)
+        end
+      end
+
+      describe "method calls" do
+        describe "method with bang (!)" do
+          let(:example_command) { example_klass.example_command! }
+
+          it "passes arguments to command" do
+            expect(example_command.attr1).to eq(example_klass)
+          end
+
+          it "returns a command instance" do
+            expect(example_command).to be_a(ExampleCommand)
+          end
+
+          it "executes command`s call!" do
+            allow_any_instance_of(ExampleCommand).to receive(:call!)
+
+            expect(example_command).to have_received(:call!)
+          end
+        end
+
+        describe "method without bang (!)" do
+          let(:example_command) { example_klass.example_command }
+
+          it "passes arguments to command" do
+            expect(example_command.attr1).to eq(example_klass)
+          end
+
+          it "returns a command instance" do
+            expect(example_command).to be_a(ExampleCommand)
+          end
+
+          it "executes command`s call" do
+            allow_any_instance_of(ExampleCommand).to receive(:call)
+
+            expect(example_command).to have_received(:call)
+          end
+        end
+      end
+    end
+  end
 end
